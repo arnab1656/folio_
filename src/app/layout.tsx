83985a28e6c29f4ca731fbbc6704c3fa54b04d66
Arnab Paul <arnab.paul.1656@gmail.com>
2025-05-05 21:10:37 +0530

    optimized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../style/globals.css";
 import { ThemeProvider } from "@/provider/ThemeProvider";
 const inter = Inter({ subsets: ["latin"] });
 
